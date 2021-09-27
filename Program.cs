10eb6d39b37c3d51637f291b684c70f978e77711
Nermienkh <nermien.khalifa@gmail.com>
2021-09-27 11:57:46 +0200

    Add new Comments

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace testDbCon
     {
         static void Main(string[] args)
         {
-
-            using ( testEntities1  context = new testEntities1()) {
+            //create dbcontext instance 
+            using ( testEntities1  context = new testEntities1()) 
+            {
+                //add new project
                 context.Projects.Add(new Project {  ID="15", Project_Name="Project15" });
+                // add  new employee assigned to pervious project 
                 context.Employees_.Add(new Employees_ {  ID="1111", Name="NNN",  ProjectID= "15"});
+                //save changes 
                 context.SaveChanges();
-            
             }
 
         }
