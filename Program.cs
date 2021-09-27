e47dcea5fc7103ddce1d33d901ccd119ad564fbc
Nermienkh <nermien.khalifa@gmail.com>
2021-09-27 11:52:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDbCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using ( testEntities1  context = new testEntities1()) {
+                context.Projects.Add(new Project {  ID="15", Project_Name="Project15" });
+                context.Employees_.Add(new Employees_ {  ID="1111", Name="NNN",  ProjectID= "15"});
+                context.SaveChanges();
+            
+            }
+
+        }
+    }
+}
